d0460eea6d3102b9668f745cb117e7c57c066225
Laurent <magrez0laurent@gmail.com>
2022-11-14 11:11:58 +0100

    initial commit, project TS & jest setup

---
+++ README.md
@@ -0,0 +1,13 @@
+## initialiser le projet :
+
+`npm i`
+
+## lancer le projet :
+
+`npm start`
+
+## Tester le projet :
+
+`npm test`
+
+.
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
+/* dans les preset changer en function : react si react...etc */
